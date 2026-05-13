f0423086506cb48a3e58562c145d1f9b55e161f1
Godfred Adjetey <godfredadjetey99@gmail.com>
2026-05-13 16:18:17 +0000

    adding more thing to make yoursite visible

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className="w-full h-screen flex items-center justify-center relative ">
     {HeroSection.map((item) => (
       <div key={item.title} className="w-full h-full relative">
-        <img src={item.images[0].value} alt={item.images[0].name} className="w-full h-full object-cover" />
+        <img src={item.images[0].value} alt={item.images[0].name} className="w-full h-full object-cover " loading="lazy" />
         <div className="absolute top-50 md:top-1/2 md:left-1/2 transform md:-translate-x-1/2 md:-translate-y-1/2 text-white bg-gray-700 opacity-75 py-6 px-15 lg:px-8 max-w-7xl rounded-md">
           <p className=" mb-4  ">The Unlimited <span className="font-playfair font-bold text-3xl">{item.title}</span> Experience</p>
           <h1 className="font-playfair text-xl md:text-5xl md:text-[56px] md-leading-[56px] font-bold md:font-extrabold max-w-xl mt-4">{item.descriptionh1}</h1>
